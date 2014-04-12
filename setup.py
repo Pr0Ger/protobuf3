a54f3edec97c5919c1faeb2291308d681049fb56
Pr0Ger <git@pr0ger.org>
2014-04-12 20:54:52 +0400

    Let's start from this

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='protobuf3',
+    version='',
+    packages=['protobuf3'],
+    url='',
+    license='MIT',
+    author='Pr0Ger',
+    author_email='me@pr0ger.prg',
+    description=''
+)
