84f1ecb5505071998ea22a09a9ec0f903215cec7
Pr0Ger <git@pr0ger.org>
2014-04-13 10:11:27 +0400

    Added decoding field signature https://developers.google.com/protocol-buffers/docs/encoding#structure

--- protobuf3/message.py
+++ protobuf3/message.py
@@ -2,8 +2,33 @@ from functools import reduce
 
 
 class Message(object):
+    FIELD_VARINT = 0
+    FIELD_FIXED64 = 1
+    FIELD_VARIABLE_LENGTH = 2
+    FIELD_START_GROUP = 3
+    FIELD_END_GROUP = 4
+    FIELD_FIXED32 = 5
+
     def __init__(self):
         pass
+
+    @staticmethod
+    def _decode_field_signature(input_iterator):
+        number = Message._decode_varint(input_iterator)
+
+        field_type = number & 0b111
+        field_number = number >> 3
+
+        if field_type == Message.FIELD_VARIABLE_LENGTH:
+            field_length = Message._decode_varint(input_iterator)
+        elif field_type in (Message.FIELD_START_GROUP, Message.FIELD_END_GROUP):
+            raise NotImplementedError("Groups is deprecated and unsupported in protobuf3")
+        elif field_type in (Message.FIELD_VARINT, Message.FIELD_FIXED64, Message.FIELD_FIXED32):
+            field_length = None
+        else:
+            raise ValueError("Unknown wire type")
+
+        return field_type, field_number, field_length
 
     @staticmethod
     def _decode_varint(input_iterator):
--- tests/test_message.py
+++ tests/test_message.py
@@ -4,6 +4,21 @@ from unittest import TestCase
 
 class TestMessage(TestCase):
 
+    def test_decode_field_signature(self):
+        tmp = Message()
+
+        data = [0b0001000]
+        self.assertEqual(tmp._decode_field_signature(iter(data)), (0, 1, None))
+
+        data = [0x12, 0x07]
+        self.assertEqual(tmp._decode_field_signature(iter(data)), (2, 2, 7))
+
+        data = [0b00001011]
+        self.assertRaises(NotImplementedError, tmp._decode_field_signature, iter(data))
+
+        data = [0b00001111]
+        self.assertRaises(ValueError, tmp._decode_field_signature, iter(data))
+
     def test_decode_varint(self):
         tmp = Message()
 
