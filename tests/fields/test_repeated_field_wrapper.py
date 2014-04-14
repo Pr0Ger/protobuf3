d15b611ba3f4d7758fd9a5ec6d8c0485777cc17f
Pr0Ger <git@pr0ger.org>
2014-04-14 19:55:24 +0400

    ListWrapper now have __len__ methods

--- tests/fields/test_repeated_field_wrapper.py
+++ tests/fields/test_repeated_field_wrapper.py
@@ -1,6 +1,24 @@
-from protobuf3.repeated_field_wrapper import RepeatedFieldWrapper
+from protobuf3.fields.string import StringField
+from protobuf3.message import Message
 from unittest import TestCase
 
 
 class TestRepeatedFieldWrapper(TestCase):
-    pass
+    def setUp(self):
+        class TestMessage(Message):
+            a = StringField(field_number=1, repeated=True)
+
+        raw_message = [
+            0x0A, 0x07, 0x74, 0x65, 0x73, 0x74, 0x69, 0x6E, 0x67,
+            0x0A, 0x08, 0x74, 0x65, 0x73, 0x74, 0x69, 0x6E, 0x67, 0x31
+        ]
+
+        self.msg = TestMessage()
+        self.msg.parse_from_bytes(raw_message)
+
+    def test_get_item(self):
+        self.assertEqual(self.msg.a[0], 'testing')
+        self.assertEqual(self.msg.a[0], 'testing1')
+
+    def test_len(self):
+        self.assertEqual(len(self.msg.a), 2)
