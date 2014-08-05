b144cc9f0a4faa3a0b6971f3937325179df7ae0a
Pr0Ger <git@pr0ger.org>
2014-08-05 16:40:54 +0400

    Bump version, proper dependencies set

--- setup.py
+++ setup.py
@@ -1,13 +1,31 @@
-from distutils.core import setup
+from setuptools import setup
+
+dependencies = []
+
+try:
+    import enum
+except ImportError:
+    dependencies.append('enum34')
+
 
 setup(
     name='protobuf3',
-    version='',
-    packages=['protobuf3', 'protobuf3.compiler', 'protobuf3.fields'],
-    scripts=['bin/protoc-gen-python3'],
-    url='',
-    license='MIT',
+    version='0.1',
     author='Pr0Ger',
     author_email='me@pr0ger.org',
-    description='Protocol buffers library for Python 3'
+    url='https://github.com/Pr0Ger/protobuf3',
+    license='MIT',
+    description='Protocol buffers library for Python 3',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    packages=['protobuf3', 'protobuf3.compiler', 'protobuf3.fields'],
+    scripts=['bin/protoc-gen-python3'],
+
+    install_requires=dependencies,
 )
