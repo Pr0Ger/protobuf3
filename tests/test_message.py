9a3534494d78e429b47a3a66eb42f41316abfec8
leovp <leovp@users.noreply.github.com>
2016-03-28 21:28:06 +0300

    Update message tests to reflect new behaviour.
    Basically parse_from_bytes() now only accepts byte-like objects,
    which, in retrospect, is a correct way of doing it. No lists are allowed,
    so tests are getting a rewrite. Also now we can't reuse (and shouldn't have)
    instances of Message to parse entirely different payloads.

--- tests/test_message.py
+++ tests/test_message.py
@@ -7,18 +7,20 @@ from unittest import TestCase
 class TestMessage(TestCase):
     def test_decode_field_signature(self):
         tmp = Message()
+        data = bytes([0b0001000])
+        self.assertEqual(tmp._decode_field_signature(data), (0, 1, None))
 
-        data = [0b0001000]
-        self.assertEqual(tmp._decode_field_signature(iter(data)), (0, 1, None))
+        tmp = Message()
+        data = bytes([0x12, 0x07])
+        self.assertEqual(tmp._decode_field_signature(data), (2, 2, 7))
 
-        data = [0x12, 0x07]
-        self.assertEqual(tmp._decode_field_signature(iter(data)), (2, 2, 7))
+        tmp = Message()
+        data = bytes([0b00001011])
+        self.assertRaises(NotImplementedError, tmp._decode_field_signature, data)
 
-        data = [0b00001011]
-        self.assertRaises(NotImplementedError, tmp._decode_field_signature, iter(data))
-
-        data = [0b00001111]
-        self.assertRaises(ValueError, tmp._decode_field_signature, iter(data))
+        tmp = Message()
+        data = bytes([0b00001111])
+        self.assertRaises(ValueError, tmp._decode_field_signature, data)
 
     def test_encode_field_signature(self):
         tmp = Message()
@@ -30,18 +32,20 @@ class TestMessage(TestCase):
 
     def test_decode_varint(self):
         tmp = Message()
+        data = bytes([0b00000000])
+        self.assertEqual(tmp._decode_varint(data), 0)
 
-        data = [0b00000000]
-        self.assertEqual(tmp._decode_varint(iter(data)), 0)
+        tmp = Message()
+        data = bytes([0b00000001])
+        self.assertEqual(tmp._decode_varint(data), 1)
 
-        data = [0b00000001]
-        self.assertEqual(tmp._decode_varint(iter(data)), 1)
+        tmp = Message()
+        data = bytes([0b10010110, 0b00000001])
+        self.assertEqual(tmp._decode_varint(data), 150)
 
-        data = [0b10010110, 0b00000001]
-        self.assertEqual(tmp._decode_varint(iter(data)), 150)
-
-        data = [0b10101100, 0b00000010]
-        self.assertEqual(tmp._decode_varint(iter(data)), 300)
+        tmp = Message()
+        data = bytes([0b10101100, 0b00000010])
+        self.assertEqual(tmp._decode_varint(data), 300)
 
     def test_encode_varint(self):
         tmp = Message()
@@ -54,34 +58,34 @@ class TestMessage(TestCase):
     def test_decode_raw_message(self):
         # FIELD_VARINT
         tmp = Message()
-        data = [0x08, 0x96, 0x01]
+        data = bytes([0x08, 0x96, 0x01])
         expected_message = {1: [WireField(type=FIELD_VARINT, value=150)]}
-        tmp._decode_raw_message(iter(data))
+        tmp._decode_raw_message(data)
         self.assertDictEqual(tmp._Message__wire_message, expected_message)
 
         # FIELD_FIXED64
         tmp = Message()
-        data = [0x09, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08]
+        data = bytes([0x09, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08])
         expected_message = {
             1: [WireField(type=FIELD_FIXED64, value=b'\x01\x02\x03\x04\x05\x06\x07\x08')]
         }
-        tmp._decode_raw_message(iter(data))
+        tmp._decode_raw_message(data)
         self.assertDictEqual(tmp._Message__wire_message, expected_message)
 
         # FIELD_VARIABLE_LENGTH
         tmp = Message()
-        data = [0x12, 0x07, 0x74, 0x65, 0x73, 0x74, 0x69, 0x6E, 0x67]
+        data = bytes([0x12, 0x07, 0x74, 0x65, 0x73, 0x74, 0x69, 0x6E, 0x67])
         expected_message = {
             2: [WireField(type=FIELD_VARIABLE_LENGTH, value=b'testing')]
         }
-        tmp._decode_raw_message(iter(data))
+        tmp._decode_raw_message(data)
         self.assertDictEqual(tmp._Message__wire_message, expected_message)
 
         # FIELD_FIXED32
         tmp = Message()
-        data = [0x0D, 0x01, 0x02, 0x03, 0x04]
+        data = bytes([0x0D, 0x01, 0x02, 0x03, 0x04])
         expected_message = {1: [WireField(type=FIELD_FIXED32, value=b'\x01\x02\x03\x04')]}
-        tmp._decode_raw_message(iter(data))
+        tmp._decode_raw_message(data)
         self.assertDictEqual(tmp._Message__wire_message, expected_message)
 
     def test_decode_incorrect_field_signature(self):
@@ -89,14 +93,14 @@ class TestMessage(TestCase):
             b = StringField(field_number=1)
 
         msg = StringTestMessage()
-        data = [0x08, 0x96, 0x01]
-        self.assertRaises(ValueError, msg._decode_raw_message, iter(data))
+        data = bytes([0x08, 0x96, 0x01])
+        self.assertRaises(ValueError, msg._decode_raw_message, data)
 
     def test_decode_repeated_field_with_different_types(self):
-        raw_message = [
+        raw_message = bytes([
             0x08, 0x96, 0x01,
             0x0D, 0x01, 0x02, 0x03, 0x04
-        ]
+        ])
 
         msg = Message()
         self.assertRaises(ValueError, msg.parse_from_bytes, raw_message)
@@ -111,7 +115,7 @@ class TestMessage(TestCase):
     def test_get_wire_values(self):
         tmp = Message()
 
-        data = [0x08, 0x96, 0x01]
+        data = bytes([0x08, 0x96, 0x01])
         tmp.parse_from_bytes(data)
         self.assertEqual(tmp._get_wire_values(1), [WireField(type=FIELD_VARINT, value=150)])
 
@@ -125,13 +129,13 @@ class TestMessage(TestCase):
     def test_parse_from_bytes(self):
         tmp = Message()
 
-        data = [0x08, 0x96, 0x01]
+        data = bytes([0x08, 0x96, 0x01])
         expected_message = {1: [WireField(type=FIELD_VARINT, value=150)]}
         tmp.parse_from_bytes(data)
         self.assertDictEqual(tmp._Message__wire_message, expected_message)
 
     def test_create_from_bytes(self):
-        data = [0x08, 0x96, 0x01]
+        data = bytes([0x08, 0x96, 0x01])
         expected_message = {1: [WireField(type=FIELD_VARINT, value=150)]}
 
         tmp = Message.create_from_bytes(data)
